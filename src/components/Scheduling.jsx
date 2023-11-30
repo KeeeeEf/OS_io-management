da9b42a51a1926ceba0976d588d5f3449d07a144
ysa <128650565+maelliecia@users.noreply.github.com>
2023-11-30 23:03:02 +0800

    Setup Scheduling component

--- src/components/Scheduling.jsx
+++ src/components/Scheduling.jsx
@@ -2,130 +2,79 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 export const Scheduling = () => {
-//   const navigate = useNavigate();
-//   const location = useLocation();
-//   const { pages, nof } = location.state;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { trackStream, ct } = location.state;
 
-//   const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
-//   const [noFrames, setNoFrames] = useState('');
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
+  const [curTrack, setCurTrack] = useState('');
 //   const [tableData, setTableData] = useState([]);
 //   const [replacedData, setReplacedData] = useState([]);
 //   const [pageFaults, setPageFaults] = useState(0);
 //   const [pageHits, setPageHits] = useState(0);
 
-//   useEffect(() => {
-//     const selectedAlgo = sessionStorage.getItem('selectedAlgorithm');
+  useEffect(() => {
+    const selectedAlgo = sessionStorage.getItem('selectedAlgorithm');
 
-//     import(/* @vite-ignore */ `./algos/${selectedAlgo.toLowerCase()}`).then((module) => {
-//       const { calculateReplacement } = module;
-//       const frames = parseInt(nof, 10);
-//       const functionCall = calculateReplacement(pages, frames);
+    import(/* @vite-ignore */ `./algos/${selectedAlgo.toLowerCase()}`).then((module) => {
+    //   const { calculateScheduling } = module;
+      const ctrack = parseInt(ct, 10);
+    //   const functionCall = calculateScheduling(trackStream, ctrack);
 
-//       const resultData = functionCall.frameResults || [];
-//       const resultReplaced = functionCall.replacedPages || [];
-//       const { pageFaults, pageHits } = functionCall;
+    //   const resultData = functionCall.frameResults || [];
+    //   const resultReplaced = functionCall.replacedPages || [];
+    //   const { pageFaults, pageHits } = functionCall;
 
-//       setSelectedAlgorithm(selectedAlgo);
-//       setNoFrames(frames);
-//       setTableData(resultData);
-//       setReplacedData(resultReplaced);
-//       setPageFaults(pageFaults);
-//       setPageHits(pageHits);
-//     });
-//   }, [pages, nof]);
+      setSelectedAlgorithm(selectedAlgo);
+      setCurTrack(ctrack);
+    //   setTableData(resultData);
+    //   setReplacedData(resultReplaced);
+    });
+  }, [trackStream, ct]);
 
-//   const handleBack = () => {
-//     navigate('/input');
-//     window.location.reload();
-//   };
+  const handleBack = () => {
+    navigate('/input');
+    window.location.reload();
+  };
 
-//   const handleSimulateAgain = () => {
-//     navigate('/', { state: { key: Date.now() } });
-//     sessionStorage.clear();
-//     window.location.reload();
-//   };
+  const handleSimulateAgain = () => {
+    navigate('/', { state: { key: Date.now() } });
+    sessionStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <div>
-        <h1>CPU Scheduling</h1>
-      {/* <div className="text-center">
-        <h1>{selectedAlgorithm} Page Replacement</h1>
-        <button onClick={handleBack} className="btn btn-primary">
-          Back to Input Parameters
-        </button>
-      </div>
-      
+        <div className="text-center">
+            <h1>{selectedAlgorithm} Disk Scheduling</h1>
+            <button onClick={handleBack} className="btn btn-primary">
+            Back to Input Parameters
+            </button>
+        </div>
       <div className='row mt-3'>
         <div className='col'>
-          <h5><b>No. of Frames:</b> {noFrames}</h5>
-          <h5><b>Total Memory References:</b> {pages.length}</h5>
-        </div>
-        <div className='col'>
-        </div>
-        <div className='col'>
+          <h5><b>Current Track:</b> {curTrack}</h5>
+          <h5><b>Total Track Access Requests:</b> {trackStream.length}</h5>
+          <h5>
+            <b>Stream of Track Access Requests: </b> 
+            {trackStream.map((page, pageIndex) => (
+                <span key={pageIndex}>
+                {pageIndex > 0 && ', '}
+                {page}
+                </span>
+            ))}
+            </h5>
         </div>
       </div>
-
+    
       <table className="table mt-4 text-center table-striped">
         <thead>
           <tr>
-            <th>Pages</th>
-            {pages.map((page, pageIndex) => (
-              <td key={pageIndex}>{page}</td>
-            ))}
+            <th></th>
+            
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th>Iterations</th>
-            {tableData.map((step, iteration) => (
-              <React.Fragment key={iteration}>
-                <td>{iteration + 1}</td>
-              </React.Fragment>
-            ))} 
-          </tr>
-          <tr><th></th></tr>
-            {Array.from({ length: noFrames }, (_, i) => (
-            <React.Fragment key={i}>
-              <tr>
-                <th>Frame {i + 1}</th>
-                {tableData.map((step, iteration) => (
-                  <td key={iteration}>
-                    {step.frames[i] !== null ? step.frames[i] : '-'}
-                  </td>
-                ))}
-              </tr>
-            </React.Fragment>
-          ))}
-          <tr>
-            <th></th>
-            {tableData.map((step, iteration) => (
-              <td key={iteration}>
-                {step.pageFault ? 'Fault' : step.pageHit ? 'Hit' : ''}
-              </td>
-            ))}
-          </tr>
-          <tr>
-            <th>Replaced Pages</th>
-            {replacedData.map((replacedData, iteration) => (
-              <React.Fragment>
-                  <td key={iteration}>{replacedData !== null ? replacedData : '-'}</td>
-              </React.Fragment>
-            ))} 
-          </tr>
-        </tbody>
       </table>
-      
-      <div className="row mt-4 text-center">
-        <div className="col">
-          <h5><b>Total Page Faults:</b> {pageFaults}</h5>
-          <h5><b>Total Page Hits:</b> {pageHits}</h5>
-        </div>
-        <div className="col">
-          <h5><b>Faults Ratio:</b> {(pageFaults / pages.length * 100).toFixed(2)}%</h5>
-          <h5><b>Hits Ratio:</b> {(pageHits / pages.length * 100).toFixed(2)}%</h5>
-        </div>
-      </div> 
 
       <div className="text-center">
         <button
@@ -135,7 +84,7 @@ export const Scheduling = () => {
           Simulate Again
         </button>
       </div>
-        */}
+        
     </div>
   );
 };
